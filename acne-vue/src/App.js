4194355ecf3ed62cf7bf76ba4c4ca1e6281a6a19
ccm32004 <chingching.ma32004@gmail.com>
2024-01-27 16:13:59 -0500

    upload and display image

--- acne-vue/src/App.js
+++ acne-vue/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [file, setFile] = useState();
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+
+       <h2>Add Image:</h2>
+       <input type="file" onChange={handleChange} />
+       <img src={file} />
+
+
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
     </div>
   );
 }
