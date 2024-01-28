13aea4be50f192f6f242395eeafe9d417bc24a0a
ccm32004 <chingching.ma32004@gmail.com>
2024-01-27 19:25:32 -0500

    centered image

--- acne-vue/src/App.js
+++ acne-vue/src/App.js
@@ -52,26 +52,12 @@ function App() {
             <img
               src={file}
               alt="Selected Image"
-              className="my-image mt-4 rounded shadow-lg text-center"
+              className="my-image mt-4 mx-auto rounded shadow-lg text-center"
             />
           )}
         </div>
     </div>
 
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-  </header>*/}
     </div>
   );
 }
