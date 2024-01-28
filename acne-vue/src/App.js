0a906e39ed9c495aba2442667975ce14c2548eba
Elizabeth Lam <elizabeth.lam099@gmail.com>
2024-01-27 20:04:31 -0500

    update app.js

--- acne-vue/src/App.js
+++ acne-vue/src/App.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import './App.css';
 import React, { useState } from "react";
 import * as tf from '@tensorflow/tfjs';
 import './model.tflite';
-=======
-import React, { useState } from "react";
-import "./App.css"; // Assuming you have additional styles in App.css
-import styles from "./index.css";
->>>>>>> a429d57a5ab3d4b9f3537038933f098625b56534
 
 function App() {
   const [file, setFile] = useState();
 
-<<<<<<< HEAD
-  const handleChange = (e) => {
-=======
   function handleChange(e) {
->>>>>>> a429d57a5ab3d4b9f3537038933f098625b56534
     const selectedFile = e.target.files[0];
 
     if (selectedFile) {
@@ -32,7 +22,6 @@ function App() {
 
       reader.readAsDataURL(selectedFile);
     }
-<<<<<<< HEAD
   };
 
   const runInterference = async () => {
@@ -80,44 +69,6 @@ function App() {
       <div>
         <TFLiteInterference />
       </div>
-=======
-  }
-
-  return (
-    <div className="App bg-red-100 h-screen p-8">
-
-      <div className="text-center flex-col items-center gap-8">
-
-        <div>
-          <h1 className="text-6xl font-bold mb-8">AcneVue</h1>
-          <h2 className="text-4xl text-red-400 mb-4">Add Image:</h2>
-        </div>
-
-        <div>
-          <label
-            className="cursor-pointer bg-red-300 text-white py-2 px-4 rounded inline-block"
-          >
-            Choose File
-            <input
-              type="file"
-              onChange={handleChange}
-              className="hidden"
-            />
-          </label>
-        </div>
-
-        <div className="self-center align-middle mt-8">
-          {file && (
-            <img
-              src={file}
-              alt="Selected Image"
-              className="my-image mx-auto rounded shadow-lg w-64 h-auto"
-            />
-          )}
-        </div>
-      </div>
-
->>>>>>> a429d57a5ab3d4b9f3537038933f098625b56534
     </div>
   );
 }
